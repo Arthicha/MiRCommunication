69958e80397e2e5a17a0b1132fde0d1009c00cd9
Daniel Fernandez <daniel_26_97@hotmail.com>
2022-11-04 15:10:38 +0100

    Changes lab

--- MiR/MiRCommunication.py
+++ MiR/MiRCommunication.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 import requests
 import json
 
@@ -28,12 +27,33 @@ class MIR:
 				if "auth" in res_dic:
 					auth_ = res_dic["auth"]
 				else:
-					raise Exception("The json have not the correct format")
+					raise Exception("The json has not the correct format")
 
 		self.headers = {'Content-Type': 'application/json', 'Authorization': auth_}
 
+		self.mission_tex_executing = "Waiting for obstacles to be removed."
+		self.states = {10: "Emergency stop", 4: "Pause", 3: "Ready", 5: "Executing", 12: "Error"}
+		self.position_type = {"L-marker entry position": 14, "L-marker": 13, "Robot position": 0}
+		self.error_type = {""}
+
 	# -----------------------------------------------------------------------------------
 	# read functions
+	def move_to_LMarker(self, point_name, move_mission_name, helper=False):
+		guid_point, _ = self.get_LMarker_point(point_name, helper)
+		return self.__move_robot_to_guid_point__(guid_point, move_mission_name)
+
+	def move_to_point(self, point_name, move_mission_name):
+		guid_point, _ = self.get_guid_point(point_name)
+		return self.__move_robot_to_guid_point__(guid_point, move_mission_name)
+
+	def start_mission_queue(self):
+		self.change_state_id(3)
+
+	def get_actions_mission(self, guid):
+		return self.read(f'missions/{guid}/actions')
+
+	def stop_mission_queue(self):
+		self.change_state_id(4)
 
 	def get_positions(self):
 		positions = self.read('positions')
@@ -43,60 +63,124 @@ class MIR:
 		missions = self.read('mission_queue')
 		print(missions)
 
+	def get_LMarker_point(self, markerName, helper=False):
+		points = self.get_positions()
+		guid = ''
+		full_point_data = {}
+
+		for point in points:
+			if point['name'] == markerName:
+				if helper and point["type_id"] == self.position_type["L-marker entry position"]:
+					guid = point['guid']
+					full_point_data = point
+					break
+				elif not helper and point["type_id"] == self.position_type["L-marker"]:
+					guid = point['guid']
+					full_point_data = point
+					break
+			pass
+
+		return guid, full_point_data
+
+	def get_guid_point(self, point_name):
+		points = self.get_positions()
+		guid = ''
+		full_point_data = {}
+
+		for point in points:
+			if point['name'] == point_name:
+				guid = point['guid']
+				full_point_data = point
+				break
+			pass
+
+		return guid, full_point_data
+
+	def get_guid_mission(self, name):
+		missions = self.get_missions()
+		guid = ''
+		full_mission_data = {}
+
+		for mission in missions:
+			if mission['name'] == name:
+				guid = mission['guid']
+				full_mission_data = mission
+				break
+			pass
+
+		return guid, full_mission_data
+
+	def post_simple_mission_queue(self, mission_id):
+		dict_post = {"mission_id": mission_id, "priority": 0}
+		return self.write('mission_queue', elements=dict_post)
+
+	def get_missions(self):
+		return self.read('missions')
+
 	def get_battery_status(self):
 		status = self.read('status')
 		battery = status['battery_percentage']
+		print(f"status: {status}")
+
 		return battery
 
-	def is_available(self,acceptance_radius=0.1):
-		available = False
-		status = self.read('status')
+	def change_state_id(self, id_robot):
+		if not (id_robot in self.states):
+			raise Exception("The id is not correct")
 
-		distance = status['distance_to_next_target']
-		v = status['velocity']['linear']
-		w = status['velocity']['angular']
+		dict_help = {"state_id": id_robot}
+		self.put('status', dict_help)
 
-		if ((distance <= acceptance_radius) and (v == 0.0) and (w == 0.0)):
-			available = True
-		return available
+	def __move_robot_to_guid_point__(self, guid_point, move_mission_name):
+		guid_mission, _ = self.get_guid_mission(move_mission_name)
+		actions = self.get_actions_mission(guid_mission)
+
+		action_guid = ''
+
+		for action in actions:
+			if action["action_type"] == "move":
+				action_guid = action["guid"]
+				break
+
+		dict_helper = {
+			"priority": 999, "parameters": [
+				{
+					"id": "position",
+					"value": guid_point
+				}
+			]
+		}
+
+		print(action_guid)
+		self.put(f'missions/{guid_mission}/actions/{action_guid}', dict_helper)
+
+		return guid_mission, guid_point
 
 	# -----------------------------------------------------------------------------------
 	# read/write MiR register
 
-	def read_register(self,i):
+	def read_register(self, i):
 		data = self.read('registers/'+str(i))
 		return data
 
-	def write_register(self,i,value):
+	def write_register(self, i, value):
 		jsondata = {"value": value}
-		status = self.write('registers/'+str(i),elements=jsondata)
+		status = self.write('registers/'+str(i), elements=jsondata)
 		return status
-
-	# -----------------------------------------------------------------------------------
-	# move MiR
-
-	def set_target(self,station_idx,point=0):
-		'''
-		set MiR target
-		input: 
-				(1) station_idx (int) : station num 
-				(2) point (int) : subpoint
-		'''
-		status = self.write_register(21,point)
-		status = self.write_register(20,station_idx)
-		return status
-
 
 	# -----------------------------------------------------------------------------------
 	# play audio
-
-	def play_audio(self,audio_idx):
-		status = self.write_register(19,audio_idx)
+	def play_audio(self, audio_idx):
+		status = self.write_register(19, audio_idx)
 		return status
 
 	# -----------------------------------------------------------------------------------
 
-	def read(self,uri,elements='',verbose=False):
+	def read(self, uri, elements=None, verbose=False):
+
+		if elements is None:
+			elements = {}
+
 		received_mess = requests.get(self.host + uri, json=elements, headers=self.headers)
 		response = {}
 
@@ -109,7 +193,11 @@ class MIR:
 
 		return response
 
-	def write(self,uri,elements='',verbose=False):
+	def write(self, uri, elements=None, verbose=False):
+
+		if elements is None:
+			elements = {}
+
 		received_mess = requests.post(self.host + uri, json=elements, headers=self.headers)
 		response = {}
 
@@ -122,7 +210,18 @@ class MIR:
 
 		return response
 
+	def put(self, uri, elements, verbose=False):
+		received_mess = requests.put(self.host + uri, json=elements, headers=self.headers)
+		response = {}
 
+		code = self.__get_code__(received_mess)
+		if verbose:
+			print(f"Code: {code}")
+
+		if code == 200:
+			response = self.__transform_mess__(received_mess)
+
+		return response
 
 	@staticmethod
 	def __transform_mess__(received_mess):
@@ -132,3 +231,4 @@ class MIR:
 	@staticmethod
 	def __get_code__(received_mess):
 		return received_mess.status_code
+
--- main.py
+++ main.py
@@ -3,27 +3,45 @@ import time
 
 auth_file = "auth.json"
 
-
 target_idx = 0
 
 if __name__ == "__main__":
-	mir = MIR(auth_file=auth_file)
+    mir = MIR(auth_file=auth_file)
 
-	print("battery: ", mir.get_battery_status())
+    # print("battery: ", mir.get_battery_status())
+    # mir.get_mission_queue()
+    # guid, full_data = mir.get_guid_mission('EiT: simple testing mission')
+    # guid, full_data = mir.get_guid_mission('EiT: move2')
+    # guid, _ = mir.move_to_point('StationA_1', 'EiT: move2')
+    guid, _ = mir.move_to_point('Home', 'EiT: move2')
+    # guid, _ = mir.move_to_point('shelfA', 'EiT: move2')
+    print(guid)
+    mir.post_simple_mission_queue(guid)
+    mir.start_mission_queue()
+    # # mir.get_mission_definition(guid)
+    # # print(full_data)
+    # mir.stop_mission_queue()
+    # print(mir.get_positions())
 
-	while(1):
-		available = mir.is_available(acceptance_radius=0.2)
-		
-		if(available):
-			mir.play_audio(1)
-			time.sleep(3)
-			mir.set_target(target_idx)
-			print('set target at',target_idx)
-			target_idx += 1
-			if target_idx >= 3:
-				target_idx = 1
-			time.sleep(5)
-		time.sleep(3)
-
-
-
+# while 1:
+# 	available = mir.is_available(acceptance_radius=0.2)
+#
+# 	if available:
+# 		mir.play_audio(1)
+# 		# time.sleep(3)
+# 		print('set target at', target_idx)
+# 		mir.set_target(target_idx)
+#
+# 		target_idx += 1
+# 		if target_idx >= 3:
+# 			target_idx = 1
+#
+# 		mir.is_available(acceptance_radius=0.2)
+#
+# 		# i = 0
+# 		# while i < 5:
+# 		# 	mir.get_battery_status()
+# 		# 	time.sleep(1)
+#
+# 		# time.sleep(5)
+# 	time.sleep(3)
